4d89f82c6180a18843a8678f2e64d29e51596c74
yn0410 <jenny783113@gmail.com>
2021-06-02 01:44:39 +0800

    update 0602

--- by-Nian/genFace/matrixFace.vhd
+++ by-Nian/genFace/matrixFace.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_unsigned.all;
 
 entity matrixFace is
 	port( clk,TFS,gameStatus : in std_logic;
-			col_s,row_s : out std_logic_vector(0 to 7));
+			col_s,row_s,col2 : out std_logic_vector(0 to 7));
 end matrixFace;
 
 architecture a of matrixFace is
@@ -15,38 +15,41 @@ begin
 		if clk'event and clk='1' then
 			if TFS='1' then--猜中(laught)
 				case cnt is
-						when "000" => col_s<="00000000";row_s<="01111111";
-						when "001" => col_s<="00100100";row_s<="10111111";
-						when "010" => col_s<="00100100";row_s<="11011111";
-						when "011" => col_s<="00100100";row_s<="11101111";
-						when "100" => col_s<="00000000";row_s<="11110111";
-						when "101" => col_s<="01000010";row_s<="11111011";
-						when "110" => col_s<="00100100";row_s<="11111101";
-						when others => col_s<="00011000";row_s<="11111110";
+						when "000" => col_s<="00000000";row_s<="10000000";
+						when "001" => col_s<="00100100";row_s<="01000000";
+						when "010" => col_s<="00100100";row_s<="00100000";
+						when "011" => col_s<="00100100";row_s<="00010000";
+						when "100" => col_s<="00000000";row_s<="00001000";
+						when "101" => col_s<="01000010";row_s<="00000100";
+						when "110" => col_s<="00100100";row_s<="00000010";
+						when others => col_s<="00011000";row_s<="00000001";
 				end case;
+				col2 <= "00000000";
 			else--沒猜中
 				if gameStatus='0' then--還有命(cry)
 					case cnt is
-							when "000" => col_s<="00000000";row_s<="01111111";
-							when "001" => col_s<="00100100";row_s<="10111111";
-							when "010" => col_s<="00100100";row_s<="11011111";
-							when "011" => col_s<="00100100";row_s<="11101111";
-							when "100" => col_s<="00000000";row_s<="11110111";
-							when "101" => col_s<="00011000";row_s<="11111011";
-							when "110" => col_s<="00100100";row_s<="11111101";
-							when others => col_s<="01000010";row_s<="11111110";
+							when "000" => col_s<="00000000";row_s<="10000000";
+							when "001" => col_s<="00100100";row_s<="01000000";
+							when "010" => col_s<="00100100";row_s<="00100000";
+							when "011" => col_s<="00100100";row_s<="00010000";
+							when "100" => col_s<="00000000";row_s<="00001000";
+							when "101" => col_s<="00011000";row_s<="00000100";
+							when "110" => col_s<="00100100";row_s<="00000010";
+							when others => col_s<="01000010";row_s<="00000001";
 					end case;
+					col2 <= "00000000";
 				else --沒命了(cry)
 					case cnt is
-							when "000" => col_s<="00000000";row_s<="01111111";
-							when "001" => col_s<="00100100";row_s<="10111111";
-							when "010" => col_s<="00100100";row_s<="11011111";
-							when "011" => col_s<="00100100";row_s<="11101111";
-							when "100" => col_s<="00000000";row_s<="11110111";
-							when "101" => col_s<="00011000";row_s<="11111011";
-							when "110" => col_s<="00100100";row_s<="11111101";
-							when others => col_s<="01000010";row_s<="11111110";
+							when "000" => col_s<="00000000";row_s<="10000000";
+							when "001" => col_s<="00100100";row_s<="01000000";
+							when "010" => col_s<="00100100";row_s<="00100000";
+							when "011" => col_s<="00100100";row_s<="00010000";
+							when "100" => col_s<="00000000";row_s<="00001000";
+							when "101" => col_s<="00011000";row_s<="00000100";
+							when "110" => col_s<="00100100";row_s<="00000010";
+							when others => col_s<="01000010";row_s<="00000001";
 					end case;
+					col2 <= "00000000";
 				end if;
 			end if;
 		end if;
--- by-Nian/matrixFace/simulation/qsim/work/matrix@face_vlg_check_tst/_primary.vhd
+++ by-Nian/matrixFace/simulation/qsim/work/matrix@face_vlg_check_tst/_primary.vhd
@@ -2,6 +2,7 @@ library verilog;
 use verilog.vl_types.all;
 entity matrixFace_vlg_check_tst is
     port(
+        col2            : in     vl_logic_vector(0 to 7);
         col_s           : in     vl_logic_vector(0 to 7);
         row_s           : in     vl_logic_vector(0 to 7);
         sampler_rx      : in     vl_logic
